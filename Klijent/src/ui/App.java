c9394847173d77f49bac52a6969ec6b70a1a49b5
unknown <uros.mrkobrada@vsar.edu.rs>
2019-12-10 23:25:40 +0100

    iskljucen resize

--- Klijent/src/ui/App.java
+++ Klijent/src/ui/App.java
@@ -26,6 +26,7 @@ public class App {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new MainFrame();
+
 			}
 		});	
 //                
--- Klijent/src/ui/MainFrame.java
+++ Klijent/src/ui/MainFrame.java
@@ -15,34 +15,40 @@ import klijent.Klijent;
 
 
 public class MainFrame extends JFrame {
-        //svi paneli koji se koriste u gui-u
+//        svi paneli koji se koriste u gui-u
 	FirstPanel firstPanel;
         SecondPanel secondPanel;
         ThirdPanel thirdPanel;
-        //klijent koji je prisutan u svim klasama
+//        klijent koji je prisutan u svim klasama
         Klijent klijent;
+
 	public MainFrame() {
 		super("Sifrovanje javnim kljucem");
+                setResizable(false);
+
 		firstPanel = new FirstPanel();
                 secondPanel = new SecondPanel();
                 thirdPanel = new ThirdPanel();
-                
                 klijent = new Klijent();
-                
+
                 //svaki panel ce imati isti klijentski pokazivac
                 firstPanel.setKlijent(klijent);
                 secondPanel.setKlijent(klijent);
                 thirdPanel.setKlijent(klijent);
                 //prvi panel je u vezi sa trecim(jer su drugacije opcije)
                 firstPanel.setThirdPanel(thirdPanel);
+                
+
 		setLayout(new BorderLayout());
                 //dodavanje na ekran
                 this.add(thirdPanel,BorderLayout.NORTH);
                 this.add(firstPanel,BorderLayout.WEST);
                 this.add(secondPanel,BorderLayout.SOUTH);
 
+                
 		setSize(725, 150);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
+                
+                this.setVisible(true);
 	}
 }
